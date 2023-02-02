2f6141b327fc1b62e88956f1b6666d920c01df7e
laurent <laurent.lesage51@gmail.com>
2023-02-02 11:58:42 +0100

    encode password userFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
-use Datetime;
 use Faker\Factory;
 use App\Entity\User;
 use App\DataFixtures\BookFixtures;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
     public const USER_REF = 'user-ref_%s';
+
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -19,7 +31,12 @@ class UserFixtures extends Fixture
         for ($i = 0; $i < 5; $i++) {
             $user = new User();
             $user->setEmail($faker->email());
-            $user->setPassword('Password');
+            $plaintextPassword = 'Password';
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
             $user->setCreatedAt(new \Datetime);
             $user->setUpdatedAt(new \Datetime);
             $user->addBook($this->getReference('book-ref_' . $i));
